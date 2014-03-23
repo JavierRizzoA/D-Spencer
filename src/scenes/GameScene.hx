75287eeda27ce3b3e7a4cfe72e0a23e989f49a48
Javier Rizzo <javierrizzoa@gmail.com>
2014-03-22 19:53:44 -0700

    GameScene has background

--- src/scenes/GameScene.hx
+++ src/scenes/GameScene.hx
@@ -8,13 +8,16 @@ import com.haxepunk.graphics.Image;
 class GameScene extends Scene {
 	public var sentryGun:entities.SentryGun;
 	public var sentryBase:Image;
+	public var background:Image;
 
 	public override function begin() {
 		sentryBase = new Image("graphics/entities/sentry.png");
+		background = new Image("graphics/backgrounds/gamebg.png");
 		sentryBase.centerOrigin();
-		sentryBase.layer = 100;
+		sentryBase.layer = 50;
+		background.layer = 100;
 		addGraphic(sentryBase, 0, HXP.halfWidth, HXP.halfHeight);
-
+		addGraphic(background);
 		sentryGun = new entities.SentryGun(HXP.halfWidth, HXP.halfHeight);
 
 		add(sentryGun);
